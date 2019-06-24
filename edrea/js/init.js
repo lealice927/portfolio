3132b2dfeaf891af2031a41b1536c4939f5fd6d9
Alice Le <lealice@gmail.com>
2019-06-23 23:38:31 -0700

    Completely removed social icons changed some header wording

--- edrea/js/init.js
+++ edrea/js/init.js
@@ -572,7 +572,7 @@ function edrea_tm_animate_text(){
 	var animateSpan			= jQuery('.edrea_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Alice Le", "a web developer"],
+			strings: ["Software Developer", "Web developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
